9b6ce8b22109d3754cb5ae46016a60696c1b6711
Keshav <keshavjagdamni@gmail.com>
2020-08-06 16:52:54 +0530

    First commit

---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Title =(props)=>{
+    return(
+        <div className='calculator-title'>
+            {props.value}
+        </div>
+    )
+}
+export default Title;
